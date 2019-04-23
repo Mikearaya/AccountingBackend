c3064ab3be09320747954fe1529a6795c9a1ee86
mikearaya <mikaelaraya12@gmail.com>
2019-04-23 18:58:18 +0300

    initial setup commit

---
+++ AccountingBackend.Commons/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountingBackend.Commons
+{
+    public class Class1
+    {
+    }
+}
---
+++ AccountingBackend.Persistance/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountingBackend.Persistance
+{
+    public class Class1
+    {
+    }
+}
