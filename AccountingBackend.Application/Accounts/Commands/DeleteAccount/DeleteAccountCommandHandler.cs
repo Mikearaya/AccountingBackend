c41ff0e4c5448b36cba2e52998d4a1bf9104f282
mikearaya <mikaelaraya12@gmail.com>
2019-05-02 19:13:05 +0300

    #874 added delete account application layer logic

--- AccountingBackend.Application/Accounts/Commands/DeleteAccount/DeleteAccountCommandHandler.cs
+++ AccountingBackend.Application/Accounts/Commands/DeleteAccount/DeleteAccountCommandHandler.cs
@@ -3,17 +3,35 @@
  * @Author:  Mikael Araya
  * @Contact: MikaelAraya12@gmail.com
  * @Last Modified By:  Mikael Araya
- * @Last Modified Time: Apr 24, 2019 5:48 PM
+ * @Last Modified Time: May 2, 2019 7:12 PM
  * @Description: Modify Here, Please 
  */
 using System.Threading;
 using System.Threading.Tasks;
+using AccountingBackend.Application.Exceptions;
+using AccountingBackend.Application.Interfaces;
 using MediatR;
 
 namespace AccountingBackend.Application.Accounts.Commands.DeleteAccount {
     public class DeleteAccountCommandHandler : IRequestHandler<DeleteAccountCommand, Unit> {
-        public Task<Unit> Handle (DeleteAccountCommand request, CancellationToken cancellationToken) {
-            throw new System.NotImplementedException ();
+        private readonly IAccountingDatabaseService _database;
+
+        public DeleteAccountCommandHandler (IAccountingDatabaseService database) {
+            _database = database;
+        }
+
+        public async Task<Unit> Handle (DeleteAccountCommand request, CancellationToken cancellationToken) {
+            var account = await _database.Account.FindAsync (request.Id);
+
+            if (account == null) {
+                throw new NotFoundException ("Account", request.Id);
+            }
+
+            _database.Account.Remove (account);
+
+            await _database.SaveAsync ();
+
+            return Unit.Value;
         }
     }
 }
--- AccountingBackend.Application/Accounts/Commands/DeleteAccount/DeleteAccountCommandValidator.cs
+++ AccountingBackend.Application/Accounts/Commands/DeleteAccount/DeleteAccountCommandValidator.cs
@@ -1,15 +1,17 @@
 /*
- * @CreateTime: Apr 24, 2019 5:44 PM
+ * @CreateTime: May 2, 2019 7:09 PM
  * @Author:  Mikael Araya
  * @Contact: MikaelAraya12@gmail.com
  * @Last Modified By:  Mikael Araya
  * @Last Modified Time: May 2, 2019 7:09 PM
  * @Description: Modify Here, Please 
  */
-using MediatR;
+using FluentValidation;
 
 namespace AccountingBackend.Application.Accounts.Commands.DeleteAccount {
-    public class DeleteAccountCommand : IRequest {
-        public string Id { get; set; }
+    public class DeleteAccountCommandValidator : AbstractValidator<DeleteAccountCommand> {
+        public DeleteAccountCommandValidator () {
+            RuleFor (x => x.Id).NotEmpty ().NotNull ();
+        }
     }
 }
