b045a57f5b75f991de82e458eef21c8fc231f76b
mikearaya <mikaelaraya12@gmail.com>
2019-05-07 10:23:42 +0300

    #975 added system lookup request handler api

---
+++ AccountingBackend.Api/Controllers/SystemLookups/SystemLookupController.cs
@@ -0,0 +1,108 @@
+/*
+ * @CreateTime: May 6, 2019 11:56 AM
+ * @Author:  Mikael Araya
+ * @Contact: MikaelAraya12@gmail.com
+ * @Last Modified By:  Mikael Araya
+ * @Last Modified Time: May 6, 2019 12:18 PM
+ * @Description: Modify Here, Please 
+ */
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AccountingBackend.Application.SystemLookups.Commands.CreateSystemLookup;
+using AccountingBackend.Application.SystemLookups.Commands.DeleteSystemLookup;
+using AccountingBackend.Application.SystemLookups.Commands.UpdateSystemLookup;
+using AccountingBackend.Application.SystemLookups.Models;
+using AccountingBackend.Application.SystemLookups.Queries.GetSystemLookup;
+using AccountingBackend.Application.SystemLookups.Queries.GetSystemLookupList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingBackend.Api.Controllers.SystemLookups {
+
+    /// <summary>
+    /// handles all crud related request linked with system lookup 
+    /// </summary>
+    [Route ("api/system-lookups")]
+    public class SystemLookupController : Controller {
+        private readonly IMediator _Mediator;
+
+        /// <summary>
+        /// initializes system lookup class every time its requested
+        /// </summary>
+        /// <param name="mediator"></param>
+        public SystemLookupController (IMediator mediator) {
+            _Mediator = mediator;
+        }
+
+        /// <summary>
+        /// returns single instance of system lookup based on the id provided in the url
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<SystemLookupViewModel>> FindSystemLookupById (int id) {
+
+            var lookup = await _Mediator.Send (new GetSystemLookupQuery () { Id = id });
+            return Ok (lookup);
+        }
+
+        /// <summary>
+        /// return list of lookup belonging to the same type specified in the url
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [HttpGet ("type")]
+        public async Task<ActionResult<IEnumerable<SystemLookupViewModel>>> GetSystemLookupList ([FromQuery] GetSystemLookupListViewByTypeQuery query) {
+            var lookup = await _Mediator.Send (query);
+            return Ok (lookup);
+        }
+
+        /// <summary>
+        /// return all the list of system lookups available in the system
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SystemLookupViewModel>>> GetSystemLookUpList () {
+            var result = await _Mediator.Send (new GetSystemLookupListQuery ());
+            return Ok (result);
+        }
+
+        /// <summary>
+        /// handles the creating of systemlook up and accepts array of system lookup defininitions
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public async Task<ActionResult> CreateSystemLookup ([FromBody] CreateSystemLookupCommand model) {
+            var result = await _Mediator.Send (model);
+
+            return StatusCode (201, result);
+        }
+
+        /// <summary>
+        /// updates  instance of arrays of system lookup 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+
+        [HttpPut ("{id}")]
+        public async Task<ActionResult> UpdateSystemLookup (int id, [FromBody] UpdateSystemLookupCommand model) {
+            var result = await _Mediator.Send (model);
+            return StatusCode (204, result);
+        }
+
+        /// <summary>
+        /// removes single instance of system lookup object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult> DeleteSystemLookup (int id) {
+            var result = await _Mediator.Send (new DeleteSystemLookupCommand () { Id = id });
+            return NoContent ();
+        }
+    }
+}
