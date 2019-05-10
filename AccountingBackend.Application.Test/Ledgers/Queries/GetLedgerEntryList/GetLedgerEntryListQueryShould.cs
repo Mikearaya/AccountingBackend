adb06f22b849455e8e134f0be28fe5c285d1dc24
mikearaya <mikaelaraya12@gmail.com>
2019-05-10 11:29:02 +0300

    #1011 added test for ledger entry view list getter application layer

---
+++ AccountingBackend.Application.Test/Ledgers/Queries/GetLedgerEntryList/GetLedgerEntryListQueryShould.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AccountingBackend.Application.Ledgers.Models;
+/*
+ * @CreateTime: May 10, 2019 11:20 AM
+ * @Author:  Mikael Araya
+ * @Contact: MikaelAraya12@gmail.com
+ * @Last Modified By:  Mikael Araya
+ * @Last Modified Time: May 10, 2019 11:28 AM
+ * @Description: Modify Here, Please 
+ */
+using AccountingBackend.Application.Ledgers.Queries.GetLedgerEntryList;
+using Xunit;
+
+namespace AccountingBackend.Application.Test.Ledgers.Queries.GetLedgerEntryList {
+    public class GetLedgerEntryListViewQueryShould : DatabaseTestBase {
+        private GetJornalEntryListViewQueryHandler handler;
+
+        public GetLedgerEntryListViewQueryShould () {
+            handler = new GetJornalEntryListViewQueryHandler (_Database);
+        }
+
+        /// <summary>
+        /// tests the return of jornal entry list view model array
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ReturnListOfJornalEntryViewSuccessfuly () {
+            GetJornalEntryListQuery query = new GetJornalEntryListQuery ();
+
+            var result = await handler.Handle (query, CancellationToken.None);
+
+            Assert.IsType<List<JornalEntryListView>> (result);
+
+        }
+
+    }
+}
