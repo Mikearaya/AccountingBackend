3d2566e8770111931cdac45893f37b61a147a8f1
mikearaya <mikaelaraya12@gmail.com>
2019-05-14 14:50:22 +0300

    #1074 added account type delete application layer handler test

---
+++ AccountingBackend.Application.Test/AccountTypes/Commands/DeleteAccountType/DeleteAccountTypeCommandHandlerShould.cs
@@ -0,0 +1,67 @@
+/*
+ * @CreateTime: May 14, 2019 2:18 PM
+ * @Author:  Mikael Araya
+ * @Contact: MikaelAraya12@gmail.com
+ * @Last Modified By:  Mikael Araya
+ * @Last Modified Time: May 14, 2019 2:49 PM
+ * @Description: Modify Here, Please 
+ */
+using System.Threading;
+using System.Threading.Tasks;
+using AccountingBackend.Application.AccountTypes.Commands.DeleteAccountType;
+using AccountingBackend.Application.Exceptions;
+using MediatR;
+using Xunit;
+
+namespace AccountingBackend.Application.Test.AccountTypes.Commands.DeleteAccountType {
+    public class DeleteAccountTypeCommandHandlerShould : DatabaseTestBase {
+
+        /// <summary>
+        /// tests the successful deletion of account type
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task DeleteAccountTypeSuccessfuly () {
+            // Arrange
+            DeleteAccountTypeCommand command = new DeleteAccountTypeCommand () {
+                Id = 6
+            };
+            DeleteAccountTypeCommandHandler handler = new DeleteAccountTypeCommandHandler (_Database);
+            // Act
+            var result = await handler.Handle (command, CancellationToken.None);
+            // Assert
+            Assert.Equal (result, Unit.Value);
+        }
+
+        /// <summary>
+        /// tests the throwing of error when attempting to delete account type that doesnt exists
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ThrowNotFoundExceptionForIdNotExists () {
+            // Arrange
+            DeleteAccountTypeCommand command = new DeleteAccountTypeCommand () {
+                Id = 100
+            };
+            DeleteAccountTypeCommandHandler handler = new DeleteAccountTypeCommandHandler (_Database);
+            // Act
+            await Assert.ThrowsAsync<NotFoundException> (() => handler.Handle (command, CancellationToken.None));
+
+        }
+
+        /// <summary>
+        /// tests the successful validation exception return when attempting to delete system defined account types
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ThrowsValidationExceptionWhenDeleteingSystemType () {
+            // Arrange
+            DeleteAccountTypeCommand command = new DeleteAccountTypeCommand () {
+                Id = 1
+            };
+            DeleteAccountTypeCommandHandler handler = new DeleteAccountTypeCommandHandler (_Database);
+            // Act
+            await Assert.ThrowsAsync<ValidationException> (() => handler.Handle (command, CancellationToken.None));
+        }
+    }
+}
--- AccountingBackend.Application/AccountTypes/Commands/DeleteAccountType/DeleteAccountTypeCommandHandler.cs
+++ AccountingBackend.Application/AccountTypes/Commands/DeleteAccountType/DeleteAccountTypeCommandHandler.cs
@@ -3,13 +3,15 @@
  * @Author:  Mikael Araya
  * @Contact: MikaelAraya12@gmail.com
  * @Last Modified By:  Mikael Araya
- * @Last Modified Time: May 14, 2019 11:07 AM
+ * @Last Modified Time: May 14, 2019 2:46 PM
  * @Description: Modify Here, Please 
  */
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using AccountingBackend.Application.Exceptions;
 using AccountingBackend.Application.Interfaces;
+using FluentValidation.Results;
 using MediatR;
 
 namespace AccountingBackend.Application.AccountTypes.Commands.DeleteAccountType {
@@ -22,12 +24,24 @@ namespace AccountingBackend.Application.AccountTypes.Commands.DeleteAccountType 
         }
 
         public async Task<Unit> Handle (DeleteAccountTypeCommand request, CancellationToken cancellationToken) {
+            var error = false;
+
+            List<ValidationFailure> validationFailures = new List<ValidationFailure> ();
+
             var accountType = await _database.AccountType.FindAsync (request.Id);
 
             if (accountType == null) {
                 throw new NotFoundException ("Account Type");
             }
 
+            if (accountType.TypeOf == 0) {
+                error = true;
+                validationFailures.Add (new ValidationFailure ("Account Type", "Can not delete account type created by system"));
+            }
+
+            if (error) {
+                throw new ValidationException (validationFailures);
+            }
             _database.AccountType.Remove (accountType);
 
             await _database.SaveAsync ();
